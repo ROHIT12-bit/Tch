006ff054b1ba2d5196a19e21aa88fda673967b32
ROHIT12-bit <reddyrohit823@gmail.com>
2025-07-21 19:11:04 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,12 +11,11 @@ class script(object):
 # Ask Doubt on telegram @KingVJ01
 
     
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
-
-⚙️ sɪᴢᴇ : {file_size}
-
-Jᴏɪɴ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/vj_botz)</b>""" 
-
+    CAPTION = f"""
+<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}</b>
+🎬 Uploaded by: <b>@ANIELITETELUGU</b>
+📣 Join us 👉 <a href='https://t.me/ANIELITETELUGU'>@ANIELITETELUGU</a>
+"""
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
